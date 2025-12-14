46347bbf75aa28059081690288495b16c32993d0
luis-octavius <luisoctavius.sc@gmail.com>
2025-12-13 21:00:11 -0300

    feat(crawler): add a file to handle the configuration of the crawler
    - add a config struct to handle the pages crawler and the concurrency
    - implement addPageVisit
    - implement setPageData to control the page data

---
+++ config.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"sync"
+)
+
+type config struct {
+	pages              map[string]PageData
+	maxPages           int
+	baseURL            *url.URL
+	mu                 *sync.Mutex
+	concurrencyControl chan struct{}
+	wg                 *sync.WaitGroup
+}
+
+func (cfg *config) addPageVisit(normalizedURL string) (isFirst bool) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+
+	_, visited := cfg.pages[normalizedURL]
+	if visited {
+		return false
+	}
+
+	cfg.pages[normalizedURL] = PageData{URL: normalizedURL}
+	return true
+}
+
+func (cfg *config) setPageData(normalizedURL string, data PageData) {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+	cfg.pages[normalizedURL] = data
+}
+
+func configure(rawBaseURL string, maxPages, maxConcurrency int) (*config, error) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse base URL: %v", err)
+	}
+
+	return &config{
+		pages:              make(map[string]PageData),
+		maxPages:           maxPages,
+		baseURL:            baseURL,
+		mu:                 &sync.Mutex{},
+		concurrencyControl: make(chan struct{}, maxConcurrency),
+		wg:                 &sync.WaitGroup{},
+	}, nil
+}
